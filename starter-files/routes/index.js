312dbe5affee4525aa715c7b1e41755277ba5a68
Tim Holt <fremont.holt@gmail.com>
2018-11-10 10:12:46 -0500

    initial commit

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -1,9 +1,23 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // Do work here
-router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+router.get("/", (req, res) => {
+  console.log("wow this is cool");
+  res.send("Hey! It works!");
+});
+
+router.get("/reverse/:name", (req, res) => {
+  const reversedName = req.params.name
+    .split("")
+    .reduce((prev, cur) => cur + prev);
+  res.send(reversedName);
+});
+
+router.get("/square/:number", (req, res) => {
+  const number = req.params.number;
+  const square = number * number;
+  res.send(square.toString());
 });
 
 module.exports = router;
